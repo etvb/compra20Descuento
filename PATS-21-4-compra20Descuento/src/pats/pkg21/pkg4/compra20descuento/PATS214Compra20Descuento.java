d208a2a71a57019b85d44909a0a47d6c27d4b6b5
Edgardo Torres Vanden Bouch <etvb66@hotmail.com>
2017-09-17 07:34:15 -0500

    agregamos al logica y terminamos

--- PATS-21-4-compra20Descuento/src/pats/pkg21/pkg4/compra20descuento/PATS214Compra20Descuento.java
+++ PATS-21-4-compra20Descuento/src/pats/pkg21/pkg4/compra20descuento/PATS214Compra20Descuento.java
@@ -5,6 +5,8 @@
 */
 package pats.pkg21.pkg4.compra20descuento;
 
+import javax.swing.JOptionPane;
+
 
 public class PATS214Compra20Descuento {
 
@@ -12,6 +14,22 @@ public class PATS214Compra20Descuento {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
+        
+        final short condicionDescuento = 300;
+        float compraTotal, subTotal, descuento;
+        
+        subTotal =Integer.parseInt( JOptionPane.showInputDialog("Ingresa la suma de la compra"));
+        
+        if(subTotal > condicionDescuento){
+            
+            descuento = subTotal * .20F;
+            compraTotal = subTotal - descuento;
+            JOptionPane.showMessageDialog(null, "Tiene DESCUENTO de: " + "$" + descuento + "Su Total a pagar es: " + "$" + compraTotal);
+        }else{
+            JOptionPane.showMessageDialog(null, "Total a pagar: " + subTotal);
+            
+        }
+            
         
         
     }
