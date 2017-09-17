10382c07ea2ab2d40d49b8c12421e0607cace52b
Edgardo Torres Vanden Bouch <etvb66@hotmail.com>
2017-09-17 07:04:59 -0500

    Archivos base

---
+++ PATS-21-4-compra20Descuento/src/pats/pkg21/pkg4/compra20descuento/PATS214Compra20Descuento.java
@@ -0,0 +1,19 @@
+/*
+ *Ejercicio 4: En la plaza se hace un 20% de descuento a los clientes, cuya
+ * compra supere los $300 pesos. ¿Cual será la cantidad que pagará una persona
+ * por su compra?
+*/
+package pats.pkg21.pkg4.compra20descuento;
+
+
+public class PATS214Compra20Descuento {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        
+        
+    }
+    
+}
